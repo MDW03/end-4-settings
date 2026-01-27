483f702f90a178a4a8254d5b1430697c3090b5e6
Ivo <sboltxz@gmail.com>
2026-01-27 18:08:41 -0300

    loc fish shell config
    ~/.config/fish/functions/

---
+++ fish_greeting.fish
@@ -0,0 +1,11 @@
+function fish_greeting
+    clear
+echo ' ▝  ▝▜  ▝▜           ▝          '
+echo '▗▄   ▐   ▐  ▗ ▗  ▄▖ ▗▄   ▄▖ ▗▗▖ '
+echo ' ▐   ▐   ▐  ▐ ▐ ▐ ▝  ▐  ▐▘▜ ▐▘▐ '
+echo ' ▐   ▐   ▐  ▐ ▐  ▀▚  ▐  ▐ ▐ ▐ ▐ '
+echo '▗▟▄  ▝▄  ▝▄ ▝▄▜ ▝▄▞ ▗▟▄ ▝▙▛ ▐ ▐ '
+    set_color normal
+    echo "" # Pula uma linha
+    fastfetch # Chama a config que já tem a sua imagem
+end
